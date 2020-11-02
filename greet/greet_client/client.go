fa38eb68d6ffda9509ac9db076549ce89cd70321
Ritesh Harjani <ritesh.harjani@gmail.com>
2020-11-02 18:45:10 +0530

    greet: Added client and server Greet implementation using Unary gRPC
    Server:
    1. GreetServiceServer is the server API for GreetService service
    The above is an interface type. Hence we need to implement this
    interface in our server object. Which I guess will then be called by
    gRPC automatically since it is attached to server struct which was
    passed while greetpb.RegisterGreetServiceServer(s, &server{})
    
    Client:
    1. GreetServiceClient is the client API for GreetService service.
    We now need to call c.Greet() and pass the req to the server.
    We in turn should get a response back from the server for this.
    
    Signed-off-by: Ritesh Harjani <ritesh.harjani@gmail.com>

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -22,10 +23,30 @@ func main() {
 	if err != nil {
 		log.Fatalf("client: could not connect: %v", err)
 	}
-
 	defer cc.Close()
 
 	// now we can create a client which will usee connection created above.
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Created client: %f\n", c)
+	//fmt.Printf("Created client: %f\n", c)
+
+	// do unary here in below
+	doUnary(c)
+
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+
+	fmt.Printf("client: Starting to a unary gRPC\n")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Ritesh",
+			LastName:  "Harjani",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("client: error while calling Greet RPC: %v\n", err)
+	}
+
+	log.Printf("client: Response from Greet: %v\n", res.Result)
 }
